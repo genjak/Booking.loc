c34afd3b0ceaabf9573e55940961f5999ae06357
genjak <genjak@gmail.com>
2022-06-14 07:53:52 +0300

    Fix filter date booking

--- app/Http/Controllers/RoomController.php
+++ app/Http/Controllers/RoomController.php
@@ -16,10 +16,23 @@ class RoomController extends Controller
     public function filter(Request $request){
       $dataCheckIn=\Request::query()['dateCheckIn'];// это костыль я знаю...
       $dataCheckOut=\Request::query()['dateCheckOut'];// это костыль я знаю...
+/*
+SELECT * FROM rooms WHERE id NOT IN(
+SELECT idRoom
+from bookings b
+WHERE 
+('2022-06-16' BETWEEN b.dateCheckIn AND b.dateCheckOut) or 
+('2022-06-17' BETWEEN b.dateCheckIn AND b.dateCheckOut) OR 
+(b.dateCheckIn between '2022-06-16' and '2022-06-17') OR 
+(b.dateCheckOut between '2022-06-16' and '2022-06-17') or
+(b.dateCheckIn > '2022-06-16' AND b.dateCheckOut < '2022-06-17')
+)/**/
+     
+     $selectSql=" rooms WHERE id NOT IN( SELECT idRoom from bookings b WHERE ('".$dataCheckIn."' BETWEEN b.dateCheckIn AND b.dateCheckOut) or ('".$dataCheckOut."' BETWEEN b.dateCheckIn AND b.dateCheckOut) OR (b.dateCheckIn between '".$dataCheckIn."' and '".$dataCheckOut."') OR (b.dateCheckOut between '".$dataCheckIn."' and '".$dataCheckOut."') or      (b.dateCheckIn > '".$dataCheckIn."' AND b.dateCheckOut < '".$dataCheckOut."') )";
 
-      $selectSql=" bookings b, rooms r WHERE '".$dataCheckIn."' NOT BETWEEN b.dateCheckIn AND b.dateCheckOut and '".$dataCheckOut."' NOT BETWEEN b.dateCheckIn AND b.dateCheckOut and b.dateCheckIn not between '".$dataCheckIn."' and '".$dataCheckOut."' AND b.dateCheckOut not between '".$dataCheckIn."' and '".$dataCheckOut."'  AND b.idRoom=r.id";
       //dump($selectSql);
-      $results = \DB::table(\DB::raw($selectSql))->SimplePaginate(15);
+      $results = \DB::table(\DB::raw($selectSql))->orderBy('number')->SimplePaginate(15);
+      //$results = \DB::table($selectSql)->get();
 
       return view('rooms',[
         'rooms' => $results,
