8ddcd6b2361cd932b2eb10584593135ae2e510e8
genjak <genjak@gmail.com>
2022-06-15 20:41:27 +0300

    Test simple
    Test routing change

---
+++ tests/Feature/RoomTest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class RoomTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function test_mainPage()
+    {
+        $response = $this->get('/');
+
+        $response->assertStatus(200);
+    }
+
+    public function test_RoomsAll()
+    {
+        $response = $this->get(route('rooms_all'));
+
+        $response->assertStatus(200);
+    }
+
+    public function test_RoomsFilter()
+    {
+        $response = $this->get(route('rooms').'?dateCheckIn=2022-06-16&dateCheckOut=2022-06-19');
+
+        $response->assertStatus(200);
+    }    
+}
